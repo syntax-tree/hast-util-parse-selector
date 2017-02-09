0369e033bc34c3e4bab17ce8734f67892b55036c
Titus Wormer <tituswormer@gmail.com>
2017-02-09 17:25:30 +0100

    Refactor to improve size
    Inspired by GH-3.

--- index.js
+++ index.js
@@ -9,26 +9,16 @@ var hash = '#'.charCodeAt(0);
 
 /* Parse a simple CSS selector into a HAST node. */
 function parse(selector) {
-  var id = null;
-  var className = [];
   var value = selector || '';
   var name = 'div';
-  var node;
-  var type = null;
+  var props = {};
   var index = -1;
-  var code;
   var length = value.length;
+  var className;
+  var type;
+  var code;
   var subvalue;
   var lastIndex;
-
-  node = {
-    type: 'element',
-    tagName: null,
-    properties: {},
-    children: []
-  };
-
-  type = null;
 
   while (++index <= length) {
     code = value.charCodeAt(index);
@@ -38,9 +28,13 @@ function parse(selector) {
 
       if (subvalue) {
         if (type === dot) {
-          className.push(subvalue);
+          if (className) {
+            className.push(subvalue);
+          } else {
+            className = props.className = [subvalue];
+          }
         } else if (type === hash) {
-          id = subvalue;
+          props.id = subvalue;
         } else {
           name = subvalue;
         }
@@ -51,15 +45,10 @@ function parse(selector) {
     }
   }
 
-  node.tagName = name;
-
-  if (id) {
-    node.properties.id = id;
-  }
-
-  if (className.length !== 0) {
-    node.properties.className = className;
-  }
-
-  return node;
+  return {
+    type: 'element',
+    tagName: name,
+    properties: props,
+    children: []
+  };
 }
